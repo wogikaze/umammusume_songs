e74a6c3e69d39a841e087b0f8c68c00e97541f05
wogikaze <kouse2319@gmail.com>
2025-03-04 08:51:28 +0900

    レスポンシブデザイン, YouTube埋め込み追加

--- src/components/SongTable.tsx
+++ src/components/SongTable.tsx
@@ -1,17 +1,18 @@
 import { Card, CardContent } from "@/components/ui/card";
-import UmaIconList from "./UmaIconList";
 import { Songs } from "./songs";
+import UmaIconList from "./UmaIconList";
+import YouTubeEmbed from "./YouTubeEmbed";
 
 const SongTable = ({ songs }: { songs: Songs }) => {
     return (
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  gap-4">
             {songs.map((song) => (
                 <Card key={song.songName + song.albumName}>
                     <CardContent className="flex flex-col gap-4 p-4">
                         <div className="text-lg font-semibold">{song.songName}</div>
                         <UmaIconList charactors={song.charactors} />
                         <div>{song.albumName}</div>
-                        {/* <YouTubeEmbed videoId={song.youtubeId} /> */}
+                        <YouTubeEmbed videoId={song.youtubeId} />
                     </CardContent>
                 </Card>
             ))}
